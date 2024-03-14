ffdd0aa89d97e8bf394b95cb64fe19d05be788f3
Nirav <niravpatelp129@gmail.com>
2024-03-14 17:20:55 -0400

    Add fabric library and implement drag and resize functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import FileBase64 from 'react-file-base64';
 function App() {
   const [file, setFile] = useState('');
   const [designLoaded, setDesignLoaded] = useState(false);
+  const [dragStart, setDragStart] = useState(null);
+  const [position, setPosition] = useState({ x: 150, y: 150 });
+  const [size, setSize] = useState({ width: 300, height: 400 });
   const tshirtImage = 'https://i.ebayimg.com/images/g/AmAAAOSwuaFZ4TcY/s-l1200.jpg';
   const canvasRef = useRef(null);
 
@@ -14,32 +17,75 @@ function App() {
     const tshirtImg = new Image();
     const designImg = new Image();
 
-    // Set crossOrigin to anonymous for CORS
     tshirtImg.crossOrigin = 'anonymous';
     designImg.crossOrigin = 'anonymous';
 
-    tshirtImg.onload = () => {
+    const draw = () => {
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.drawImage(tshirtImg, 0, 0, canvas.width, canvas.height);
       if (designLoaded) {
-        context.drawImage(designImg, 150, 150, 300, 400);
+        context.drawImage(designImg, position.x, position.y, size.width, size.height);
       }
     };
 
+    tshirtImg.onload = draw;
     tshirtImg.src = tshirtImage;
 
     if (file) {
       designImg.onload = () => {
         setDesignLoaded(true);
-        context.drawImage(designImg, 150, 150, 300, 400);
+        draw();
       };
       designImg.src = file;
     }
-  }, [file, designLoaded]);
+
+    const handleMouseDown = (e) => {
+      const rect = canvas.getBoundingClientRect();
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+      if (
+        mouseX > position.x &&
+        mouseX < position.x + size.width &&
+        mouseY > position.y &&
+        mouseY < position.y + size.height
+      ) {
+        setDragStart({ x: mouseX - position.x, y: mouseY - position.y });
+      } else {
+        setDragStart(null);
+      }
+    };
+
+    const handleMouseMove = (e) => {
+      if (dragStart) {
+        const rect = canvas.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+        setPosition({
+          x: mouseX - dragStart.x,
+          y: mouseY - dragStart.y,
+        });
+        draw();
+      }
+    };
+
+    const handleMouseUp = () => {
+      setDragStart(null);
+    };
+
+    canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener('mousedown', handleMouseDown);
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [file, designLoaded, position, dragStart, size]);
 
   const handleExport = () => {
     const canvas = canvasRef.current;
-    canvas.toBlob(function (blob) {
+    canvas.toBlob((blob) => {
       saveAs(blob, 'custom-t-shirt.png');
     });
   };
@@ -52,11 +98,7 @@ function App() {
         ref={canvasRef}
         width={600}
         height={700}
-        style={{
-          border: '1px solid #ccc',
-          margin: '20px auto',
-          display: 'block',
-        }}
+        style={{ border: '1px solid #ccc', margin: '20px auto', display: 'block' }}
       ></canvas>
       <button onClick={handleExport}>Export</button>
     </div>
